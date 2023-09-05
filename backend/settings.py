d134e0f344b5cc9497d6fb796aaff8f57590d3dd
Hannana <105469888+hancrysta1@users.noreply.github.com>
2023-09-06 00:39:55 +0900

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'aboutPills_db',
     #     'USER': 'hansj',
-    #     'PASSWORD':'0568',
+    #     'PASSWORD':'',
     #     'HOST':'127.0.0.1',
     #     'PORT':'3306',
     # }
